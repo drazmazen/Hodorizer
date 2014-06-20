1ba561463ed3df813e5b568c0063d8c4736dfeb1
Drazen Jankovic <drazen.jankovic@gmail.com>
2014-06-20 22:04:43 +0200

    Initial commit.

---
+++ Hodor.Service/HodorService.cs
@@ -0,0 +1,88 @@
+﻿using Hodor.Libraries.CSharpHodor;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hodor.Service
+{
+    public class HodorService : IHodorService
+    {
+        BackgroundWorker worker = new BackgroundWorker();
+        IntPtr context;
+        private static bool _dehodorize = false;
+        int device;
+        Interception.Stroke stroke = new Interception.Stroke();
+        List<ushort> hodorList = new List<ushort>() { ScanCode.H, ScanCode.O, ScanCode.D, ScanCode.O, ScanCode.R, ScanCode.Space };
+        List<ushort> passKeysList = new List<ushort>() { ScanCode.Backspace, ScanCode.Enter };
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public void Hodorize()
+        {
+            worker.DoWork += worker_DoWork;
+            _dehodorize = false;
+
+            context = Interception.CreateContext();
+
+            Interception.SetFilter(context, Interception.IsKeyboard, Interception.Filter.KeyDown);
+
+            worker.RunWorkerAsync();
+            
+        }
+
+        void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int i = 0;
+            while (Interception.Receive(context, device = Interception.Wait(context), ref stroke, 1) > 0)
+            {
+                if (_dehodorize)
+                {
+                    Interception.Send(context, device, ref stroke, 1);
+                    break;
+                }
+
+                if (!passKeysList.Contains(stroke.key.code))
+                {
+                    stroke.key.code = hodorList[i % 6];                    
+                }
+                else
+                {
+                    i = 5;
+                }
+                
+                Interception.Send(context, device, ref stroke, 1);
+                i++;
+            }
+            Interception.DestroyContext(context);
+        }
+
+        public void Dehodorize()
+        {            
+            _dehodorize = true;
+        }
+
+
+
+    }
+}
---
+++ Hodor.WinService/HodorWinService.cs
@@ -0,0 +1,45 @@
+﻿using Hodor.Service;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hodor.WinService
+{
+    public partial class HodorWinService : ServiceBase
+    {
+        public ServiceHost serviceHost = null;
+        public HodorWinService()
+        {
+            ServiceName = "HodorWinService";
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            if (serviceHost != null)
+            {
+                serviceHost.Close();
+            }
+
+            serviceHost = new ServiceHost(typeof(HodorService));
+
+            serviceHost.Open();
+        }
+
+        protected override void OnStop()
+        {
+            if (serviceHost != null)
+            {
+                serviceHost.Close();
+                serviceHost = null;
+            }
+        }
+    }
+}
