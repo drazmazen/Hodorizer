c313de2bf4fc21f5d5f2a44d937c1a19c6da539d
Drazen Jankovic <drazen.jankovic@gmail.com>
2014-06-21 13:55:26 +0200

    Added http endpoint.

--- Hodor.Client/MainWindow.xaml.cs
+++ Hodor.Client/MainWindow.xaml.cs
@@ -21,7 +21,7 @@ namespace Hodor.Client
     /// </summary>
     public partial class MainWindow : Window
     {
-        HodorServiceClient svc = new HodorServiceClient("netTcpEndpoint");
+        HodorServiceClient svc = new HodorServiceClient("BasicHttpBinding_IHodorService");
         public MainWindow()
         {
             InitializeComponent();
